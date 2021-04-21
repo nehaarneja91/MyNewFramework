8dcd392ff6718fad8a6fb4ac58397fc3195e3c16
Neha <narnh@allstate.com>
2021-04-22 02:01:50 +0530

    podspec added

---
+++ MyNewFramework.framework/Headers/FWInitializer.swift
@@ -0,0 +1,14 @@
+//
+//  FWInitializer.swift
+//  MyNewFramework
+//
+//  Created by Arneja, Neha on 22/04/21.
+//
+
+import Foundation
+
+public class FWInitializer {
+    public static func displayText() {
+        print("display Text")
+    }
+}
---
+++ MyNewFramework.framework/Headers/MyNewFramework.h
@@ -0,0 +1,18 @@
+//
+//  MyNewFramework.h
+//  MyNewFramework
+//
+//  Created by Arneja, Neha on 22/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyNewFramework.
+FOUNDATION_EXPORT double MyNewFrameworkVersionNumber;
+
+//! Project version string for MyNewFramework.
+FOUNDATION_EXPORT const unsigned char MyNewFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyNewFramework/PublicHeader.h>
+
+
